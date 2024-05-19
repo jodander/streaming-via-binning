0a29ca46af35027a5843e877ba2e4757f7736ab1
Joel Andersson <joel.dan.andersson@gmail.com>
2024-05-19 13:17:49 +0200

    Improved implementation of 'R' computation + misc

--- efficient_bennett.py
+++ efficient_bennett.py
@@ -46,21 +46,6 @@ def bennett_matrix(n):
         entry_val *= (1.0 - 0.5/(k+1))
     return mat
 
-def amir_matrix(n, decay, space=None, always_mult=False):
-
-    if space == None:
-        space = math.ceil(math.log2(n))
-
-    mat = np.zeros(shape=(n,n), dtype=float)
-    entry_val = 1
-    for k in range(space):
-        np.fill_diagonal(mat[k:,:], entry_val)
-        if always_mult or k > space // 2:
-            entry_val *= (1.0 - 0.5/(k+1)) * decay
-        else:
-            entry_val *= (1.0 - 0.5/(k+1))
-    return mat
-
 # NOTE: Much, much slower. But serves as a sanity check.
 def _bennett_matrix_via_sqrtm(T):
     return scipy.linalg.sqrtm( counting_matrix(T) )
@@ -76,97 +61,6 @@ def bennett_constant_k_old(k):
     return output
 def bennett_constant_k(k):
     return math.comb(2*k, k) / math.pow(4, k)
-
-# Crude starting point.
-def approx_matrix(A, threshold_f, mode='mean', verbose=False, perform_extra_checks=False):
-    ''' Approximate a lower-triangular matrix using our dyadic-interval-based approach.
-
-    NOTE: Modifies A in-place.
-
-    Parameters:
-    A: A lower-triangular matrix.
-    threshold_f: A function that takes an integer 'i' as input and outputs a threshold 't' where
-        if the right-endpoint of a dyadic interval of size '2^i' satisfies, then all values in
-        the interval are set to the same with rule derived from 'mode',
-    mode: The rule for how all values covered by an interval are to be approximated.
-        Options:
-        1) 'left' : set to the left-endpoint.
-        2) 'right' : set to the right-endpoint.
-        3) 'mean' : set to the mean of all entries in the interval.
-        Default: 'mean'
-    verbose: Activate more printing. Default: 'False'
-    perform_extra_checks: If 'True', it verifies that the returned matrix satisfies all criteria
-        we expect from the computation. Default: 'False'
-    
-    Returns:
-        The final matrix, and an integer representing the space usage.
-    '''
-
-    # Enforce it being a square matrix for now.
-    assert A.shape[0] == A.shape[1]
-    for i in range(A.shape[0]):
-        A[i, :i+1] = approx_row(A[i, :i+1], threshold_f, mode)
-
-    if perform_extra_checks:
-        is_aligned, space_usage, intervals_are_dyadic = space_check.verify_efficient_structure(A)
-        assert is_aligned, "The rows in the matrix are not aligned to allow for streaming"
-        assert intervals_are_dyadic, "While aligned, the intervals are not dyadic."
-    else:
-        space_usage = space_check.get_space_usage_only(A)
-
-    if verbose:
-        print(f"Uses space={space_usage}")
-    
-    return A, space_usage
-
-def approx_bennett_mm(n, threshold_f, mode='mean', verbose=False, perform_extra_checks=False):
-    ''' Approximate matrix mechanism induced by the 'n' by 'n' Bennet matrix.
-
-    Parameters:
-    n: Integer representing the dimension of the matrix.
-    threshold_f: A function that takes an integer 'i' as input and outputs a threshold 't' where
-        if the right-endpoint of a dyadic interval of size '2^i' satisfies, then all values in
-        the interval are set to the same with rule derived from 'mode',
-    mode: The rule for how all values covered by an interval are to be approximated.
-        Options:
-        1) 'left' : set to the left-endpoint.
-        2) 'right' : set to the right-endpoint.
-        3) 'mean' : set to the mean of all entries in the interval.
-        Default: 'mean'
-    verbose: Activate more printing. Default: 'False'
-    perform_extra_checks: If 'True', it verifies that the returned matrix satisfies all criteria
-        we expect from the computation. Default: 'False'
-    
-    Returns:
-        Returns matrices L and R s.t. L@R the counting matrix, and an integer representing the space usage.
-    '''
-
-    # A = B**2
-    # Approximate B as L using threshold_f, then compute the resulting R
-
-    # Approximate 'B' as our 'L'
-    L, space_req = approx_matrix(bennett_matrix(n), threshold_f, mode, verbose, perform_extra_checks)
-
-    # Compute the corresponding R
-    R = scipy.linalg.inv(L) @ counting_matrix(n)
-
-    return L, R, space_req
-
-def upper_bound_sensitivity(L, sum_N_terms=None):
-
-
-    n = L.shape[0]
-    if sum_N_terms == None:
-        sum_N_terms = n
-
-    B = bennett_matrix(n)
-    P = L - B
-    Q = -np.linalg.inv(B) @ P
-
-    M = sum( np.linalg.matrix_power(Q, k) for k in range(sum_N_terms) )
-    print(np.linalg.matrix_power(Q, n-10))
-
-    return np.linalg.norm(M, ord=2)
 
 def get_variance(L, R):
     """ Returns a vector of the variance from running the matrix mechansims based on A = LR
@@ -203,6 +97,9 @@ def get_mse(L, R):
     """
     return get_variance(L, R).mean()
 
+def get_max_rse(L, R):
+    return math.sqrt(get_variance(L, R).max())
+
 def compute_sensitivity(R):
     ''' Computes the sensitivity of a matrix mechanism where 'R' is the encoder matrix
     and we are concerned with rho-zCDP.
@@ -215,7 +112,6 @@ def compute_sensitivity(R):
         L2-norm over all columns of 'R'.
     '''
     return np.linalg.norm(R, axis=0, ord=2).max()
-
 
 def approx_row(r, threshold_f, mode='mean'):
     ''' Approximates a row of a matrix.
@@ -273,13 +169,17 @@ def approx_row(r, threshold_f, mode='mean'):
 
 # TODO: Consider having to do multiple merges.
 # Intervals are in order [(t, t), (t-1, s) ..., (q, 0)]
-def merge_intervals(intervals, r, c, s):
+def merge_intervals(intervals, r, c, w):
+
+    # Default to checking all elements
+    if w is None:
+        w = 0
 
     # First, compute the new interval list after a merge
     new_intervals = []
     idx = 0
     # Store exactly the first 's' values
-    while idx < s and idx < len(intervals):
+    while idx < w and idx < len(intervals):
         new_intervals.append(intervals[idx])
         idx += 1
 
@@ -330,55 +230,6 @@ def merge_intervals(intervals, r, c, s):
     
     return new_intervals
 
-# TODO: Consider having to do multiple merges.
-# Intervals are in order [(t, t), (t-1, s) ..., (q, 0)]
-#def merge_intervals(intervals, r, c, s):
-#
-#    # First, compute the new interval list after a merge
-#    new_intervals = []
-#    idx = 0
-#    # Store exactly the first 's' values
-#    while idx < s and idx < len(intervals):
-#        new_intervals.append(intervals[idx])
-#        idx += 1
-#
-#    # Greedily merge from then on
-#    while idx < len(intervals) - 1:
-#        curr_start, curr_end = intervals[idx]
-#
-#        # Check the smallest value over the largest in interval
-#        val = r[curr_end] / r[curr_start]
-#
-#        # If the current interval has a good ratio, continue to the next interval
-#        if val < c:
-#            # Assert that the interval was not too greedily merged in the past
-#            assert val > c ** 2
-#            idx += 1
-#            new_intervals.append((curr_start, curr_end))
-#            continue
-#
-#        # We can now consider merging it.
-#        next_start, next_end = intervals[idx + 1]
-#
-#        new_val = r[next_end] / r[curr_start]
-#        if new_val > c ** 2:
-#            # Merge it.
-#            new_intervals.append((curr_start, next_end))
-#            idx += 2
-#            continue
-#        
-#        # If we got here, the current interval is a candidate for merging, but the merge is too aggressive
-#        # Skip merging and continue
-#        idx += 1
-#        new_intervals.append((curr_start, curr_end))
-#
-#    # Make sure not to skip the last interval.
-#    if len(new_intervals) == 0 or (new_intervals[-1][1] != intervals[-1][1]):
-#        new_intervals.append(intervals[-1])
-#    
-#    return new_intervals
-
-# NOTE:
 def _approximation_rule(r, idx_start, idx_end, mode):
 
     val = None
@@ -399,7 +250,7 @@ def intervals_to_row(intervals, r, mode):
     
     return r
 
-def approx_matrix_v2(A, c, s):
+def approx_matrix(A, c, w, perform_extra_checks=False):
 
     space_requirement = 0
     assert A.shape[0] == A.shape[1]
@@ -407,23 +258,23 @@ def approx_matrix_v2(A, c, s):
     for i in range(A.shape[0]):
         # Compute intervals after a merge
         intervals.insert(0, (i, i))
-        intervals = merge_intervals(intervals, A[i, :i+1], c, s)
+        intervals = merge_intervals(intervals, A[i, :i+1], c, w)
         space_requirement = max(space_requirement, len(intervals))
 
         # Use the intervals to express the row
         A[i, :i+1] = intervals_to_row(intervals, A[i, :i+1], mode='mean')
 
+    if perform_extra_checks:
+        is_aligned, space, _ = space_check.verify_efficient_structure(A)
+        assert is_aligned, "The rows in the matrix are not aligned to allow for streaming"
+        assert space_requirement == space, "Conflicting results for space needed"
+
     return A, space_requirement
 
-def approx_bennett_mm_v2(n, c, s):
-    L, space_requirement = approx_matrix_v2(bennett_matrix(n), c, s)
+def approx_bennett_mm(n, c, w=None, perform_extra_checks=False):
+    L, space_requirement = approx_matrix(bennett_matrix(n), c, w)
     # Compute the corresponding R
-    R = scipy.linalg.inv(L) @ counting_matrix(n)
+    R = scipy.linalg.solve_triangular(L, counting_matrix(n), lower=True)
 
     return L, R, space_requirement
-
-def new_approx(n, c):
-
-    B = bennett_matrix(n)
-
     
